f34eb84072adb2b0475ae951582f04d7673be6e5
Chris Durden <cdurden@gmail.com>
2022-03-05 21:40:53 -0600

    Renamed files

---
+++ loader.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const url = new URL(window.location.href);
+    const searchParams = new URLSearchParams(window.location.search);
+    const moduleUrl = searchParams.get("module") ?? "./default.js";
+    import(moduleUrl).then(function (module) {
+        module["main"]();
+    });
+});
